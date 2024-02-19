a370c7dcefea8a26b15c32e038b28e9ce7068c12
NaerHdz <36934563+NaerHdz@users.noreply.github.com>
2024-02-18 21:07:57 -0600

    UPD: Object not used eliminated

--- src/components/CardCharacter.tsx
+++ src/components/CardCharacter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet, PixelRatio} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 import {Character} from '../types/types';
 
 type Props = {
